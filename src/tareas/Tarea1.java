e79afd0e16a31e8a39d86a95a17df6ec3d0dee04
Anthony Davis Yeren Martinez <ayerenmartinez@hotmail.com>
2023-07-22 16:57:18 -0500

    HOLA MUNDO EN JAVA

--- src/tareas/Tarea1.java
+++ src/tareas/Tarea1.java
@@ -1,4 +1,9 @@
 package tareas;
 
 public class Tarea1 {
+    public static void main(String[] args) {
+        /*Crear una clase java e imprimir tu nombre en la salida o consola.*/
+
+        System.out.println("Anthony Davis, Yeren Martinez");
+    }
 }
