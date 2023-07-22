b022ae845214871db013a8b25a0c7e11c25a36be
Anthony Davis Yeren Martinez <ayerenmartinez@hotmail.com>
2023-07-22 17:19:09 -0500

    HOLA MUNDO EN JAVA

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -1,5 +1,7 @@
 public class HolaMundo {
     public static void main(String[] args) {
-        System.out.println("Hola Mundo desde Java");
+        String saludar = "Hola Mundo desde Java";
+        System.out.println(saludar);
+        System.out.println("saludar.toUpperCase() = " + saludar.toUpperCase());
     }
 }
