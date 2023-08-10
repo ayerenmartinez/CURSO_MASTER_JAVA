a50f8ef7c82a435d8b6811bb6744c228944a35aa
Anthony Davis Yeren Martinez <ayerenmartinez@hotmail.com>
2023-08-10 01:21:40 -0500

    HOLA MUNDO EN JAVA

---
+++ src/EjemploStringTestRendimientoConcat.java
@@ -0,0 +1,20 @@
+public class EjemploStringTestRendimientoConcat {
+    public static void main(String[] args) {
+        String a = "a";
+        String b = "b";
+        String c = "c";
+        StringBuilder sb = new StringBuilder(a); //StringBuilder es mutable
+        long inicio = System.currentTimeMillis();
+        long fin = System.currentTimeMillis();
+
+        for (int i=0;i<500;i++){
+            c = c.concat(a).concat(b).concat("\n"); // 500 => 2ms, 1000 => 4ms, 10000 => 105ms, 100000 => 5071ms
+            c += a + b + "\n"; // 500 => 19, 1000 => 21ms, 10000 => 68, 100000 = 1976ms
+            sb.append(a).append(b).append("\n"); // 500 => 0ms, 1000 => 0ms, 10000 => 2ms, 100000 => 8ms
+        }
+
+        System.out.println(fin-inicio);
+        System.out.println("c = " + c);
+        System.out.println("sb = " + sb.toString());
+    }
+}
---
+++ src/EjemploStringValidar.java
@@ -0,0 +1,2 @@
+public class EjemploStringValidar {
+}
