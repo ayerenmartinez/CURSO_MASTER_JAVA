b46f18b3cf1e0544e067adc01e889e43d043555e
Anthony Davis Yeren Martinez <ayerenmartinez@hotmail.com>
2023-08-13 01:05:01 -0500

    HOLA MUNDO EN JAVA

---
+++ src/OperadorAritmetico.java
@@ -0,0 +1,31 @@
+import javax.swing.*;
+
+public class OperadorAritmetico {
+    public static void main(String[] args) {
+        int i = 5;
+        int j = 4;
+        int suma = i + j;
+        System.out.println("suma = " + suma);
+        int resta = i - j;
+        System.out.println("resta = "+resta);
+        System.out.println("(i-j) ="+(i-j));
+        int multi = i * j;
+        System.out.println("multiplicación = "+multi);
+        int div = i / j;
+        float div2 = (float) i / (float) j;
+        System.out.println("div = "+div);
+        System.out.println("div2 = " + div2);
+
+        int resto = i % j;
+        System.out.println("resto = " + resto);
+        resto = 8 % 5;
+        System.out.println("resto = " + resto);
+
+        int numero = Integer.parseInt(JOptionPane.showInputDialog("Ingrese un Número: "));
+        if(numero % 2 ==0){
+            System.out.println("numero par = "+numero);
+        }else{
+            System.out.println("numero impar = "+numero);
+        }
+    }
+}
