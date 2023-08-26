ddf51cb92bc81ecf0f5d551509f99511a48e56f0
Anthony Davis Yeren Martinez <ayerenmartinez@hotmail.com>
2023-08-26 00:19:26 -0500

    HOLA MUNDO EN JAVA

---
+++ src/SentenciasBucleEtiquetasBuscar.java
@@ -0,0 +1,37 @@
+public class SentenciasBucleEtiquetasBuscar {
+    public static void main(String[] args) {
+        buscarUnaPalabraEnLaFrase();
+    }
+
+    public static void buscarUnaPalabraEnLaFrase(){
+        String frase = "tres tristes tigres tragan trigo en un trigal";
+        String palabra = "trigo";
+        int maxFrase = frase.length();
+        int maxPalabra = palabra.length();
+        int cantidad = 0;
+
+        buscar: for(int i = 0; i < maxFrase; i++){
+            int k = i;
+            for (int j = 0;j < maxPalabra; j++){
+                if(frase.charAt(k++) != palabra.charAt(j)){
+                    continue buscar;
+                }
+            }
+        }
+        System.out.println("Encontrado = " + cantidad + " veces la palabra");
+    }
+
+    public static void buscarUnCaracter(){
+        String frase = "tres tristes tigres tragan trigo en un trigal";
+        int max = frase.length();
+        int cont = 0;
+        char letra = 'g';
+        for(int i = 0; i < max; i++){
+            if(frase.charAt(i) != letra){
+                continue;
+            }
+            cont++;
+        }
+        System.out.println("Encontrado = " + cont + " veces el caracter '"+letra+"' en la frase");
+    }
+}
