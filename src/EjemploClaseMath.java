fa793ab1ebef45ff31c5bc4d144a47221632a2e1
Anthony Davis Yeren Martinez <ayerenmartinez@hotmail.com>
2023-11-10 23:25:16 -0500

    Clase Math y Clase Runtime

---
+++ src/EjemploClaseMath.java
@@ -0,0 +1,15 @@
+public class EjemploClaseMath {
+    public static void main(String[] args) {
+        int absoluto = Math.abs(-3);
+        System.out.println("absoluto = " + absoluto);
+
+        absoluto = Math.abs(3);
+        System.out.println("absoluto = " + absoluto);
+
+        double max = Math.max(3.5,1.2);
+        System.out.println("max = " + max);
+
+        double min = Math.min(3.5,1.2);
+        System.out.println("min = " + min);
+    }
+}
