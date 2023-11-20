ca5a7e6c5a061502c5cd6a05ff1d8fd1be3f5901
Anthony Davis Yeren Martinez <ayerenmartinez@hotmail.com>
2023-11-20 07:12:48 -0500

    Clase Math y Clase Runtime

--- src/EjemploArreglos.java
+++ src/EjemploArreglos.java
@@ -1,11 +1,53 @@
+import java.util.Arrays;
+
 public class EjemploArreglos {
     public static void main(String[] args) {
+        //el corchete al declarar un arreglo puede ir en el tipo o en la variable
+        String[] productos = new String[7];
+        productos[0] ="Kingston Pendrive 64gb";
+        productos[1] ="Samsung Galaxy";
+        productos[2] ="Disco Durso SSD Samsung Externo";
+        productos[3] ="Asus Notebook";
+        productos[4] ="Macbook Air";
+        productos[5] ="Chromecast 4ta generación";
+        productos[6] ="Bicleta Oxford";
+        Arrays.sort(productos);
+
+        /*
+        System.out.println("productos[0] = " + productos[0]);
+        System.out.println("productos[1] = " + productos[1]);
+        System.out.println("productos[2] = " + productos[2]);
+        System.out.println("productos[3] = " + productos[3]);
+        System.out.println("productos[4] = " + productos[4]);
+        System.out.println("productos[5] = " + productos[5]);
+        System.out.println("productos[6] = " + productos[6]);
+        */
+
+
+        String prod1 = productos[0];
+        String prod2 = productos[1];
+        String prod3 = productos[2];
+        String prod4 = productos[3];
+        String prod5 = productos[4];
+        String prod6 = productos[5];
+        String prod7 = productos[6];
+
+        System.out.println("productos[0] = " + prod1);
+        System.out.println("productos[1] = " + prod2);
+        System.out.println("productos[2] = " + prod3);
+        System.out.println("productos[3] = " + prod4);
+        System.out.println("productos[4] = " + prod5);
+        System.out.println("productos[5] = " + prod6);
+        System.out.println("productos[6] = " + prod7);
+
         int[] numeros = new int[4];
 
         numeros[0] = 1;
         numeros[1] = 2;
         numeros[2] = 3;
         numeros[3] = 4;
+
+        Arrays.sort(numeros);
 
         int i = numeros[0];
         int j = numeros[1];
