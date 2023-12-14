33bc4e6b43c93acefb9e64eda0575ccbe7ea91fe
Anthony <ayerenmartinez@hotmail.com>
2023-12-14 18:57:06 -0500

    Eliminacion de un Elemento de un Arreglo y resolución de la tarea 10.

---
+++ src/EjemploArreglosEliminarElemento.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class EjemploArreglosEliminarElemento {
+    public static void main(String[] args) {
+        int[] a = new int[10];
+        Scanner scanner = new Scanner(System.in);
+
+        for (int i = 0; i < a.length; i++) {
+            System.out.print("Ingrese un número: ");
+            a[i] = scanner.nextInt();
+        }
+        System.out.println();
+        System.out.println("Ingrese una posición a eliminar entre 0-9: ");
+        int posicion = scanner.nextInt();
+
+        for (int i = posicion; i < a.length-1; i++) {
+            a[i] = a[i+1];
+        }
+
+        //se crea array b para eliminar la ultima posicion y no salga repetido
+        int[] b = new int[a.length-1];
+        System.arraycopy(a,0,b,0,b.length);
+
+        a = b; //paso el array b a array a, con eso el arrat b sirve como un array intermedio de ayuda
+
+        for (int i = 0; i < b.length; i++) {
+            System.out.println(i + " => "+ b[i]);
+        }
+    }
+}
