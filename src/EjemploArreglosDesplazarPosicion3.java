dd06ce2c68eed1c2eb539ac0ac191162d6186de0
Anthony <ayerenmartinez@hotmail.com>
2023-12-14 18:16:37 -0500

    Desplazar una posición cada elemento de un arreglo 3

---
+++ src/EjemploArreglosDesplazarPosicion3.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class EjemploArreglosDesplazarPosicion3 {
+    public static void main(String[] args) {
+        int[] a = new int[7];
+        int numero, posicion;
+
+        Scanner scanner = new Scanner(System.in);
+
+        //se pone -1 en el "a.length" ya que se tiene que reservar la ultima posicion para el desplazamiento
+        for (int i = 0; i < a.length-1; i++) {
+            System.out.print("Ingrese un número: ");
+            a[i] = scanner.nextInt();
+        }
+        System.out.println();
+        System.out.println("Ingrese un número a insertar: ");
+        numero = scanner.nextInt();
+        posicion= 0;
+
+        while (posicion < 6 && numero > a[posicion]){
+            posicion++;
+        }
+
+        for(int i = a.length -2 ;i >= posicion;i--){
+            a[i+1] = a[i];
+        }
+        a[posicion] = numero;
+        System.out.println("El nuevo arreglo ordenado: ");
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(i+"=>"+a[i]);
+        }
+
+    }
+}
