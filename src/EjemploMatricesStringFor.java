33dae3c427e6503c6e5db291cf634bbfce8fe81f
Anthony <ayerenmartinez@hotmail.com>
2023-12-15 10:28:30 -0500

    Matriz, declaración y acceder a sus valores usando for y foreach

---
+++ src/EjemploMatricesStringFor.java
@@ -0,0 +1,26 @@
+public class EjemploMatricesStringFor {
+    public static void main(String[] args) {
+        String[][] nombres= new String[3][2];
+        nombres[0][0] = "Pepe";
+        nombres[0][1] = "Pepa";
+        nombres[1][0] = "Josefa";
+        nombres[1][1] = "Paco";
+        nombres[2][0] = "Lucas";
+        nombres[2][1] = "Pancha";
+
+        System.out.println("Iterando con for");
+        for (int f = 0; f < nombres.length; f++) {
+            for (int c = 0; c < nombres[f].length; c++) {
+                System.out.print(nombres[f][c]+"\t");
+            }
+            System.out.println();
+        }
+        System.out.println("Iterando con foreach");
+        for (String[] fila: nombres) {
+            for (String nombre: fila) {
+                System.out.print(nombre + "\t");
+            }
+            System.out.println();
+        }
+    }
+}
