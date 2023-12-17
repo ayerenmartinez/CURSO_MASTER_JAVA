90409a2d0ad8bc7d46283393de63b8c8fb347984
Anthony <ayerenmartinez@hotmail.com>
2023-12-17 13:36:48 -0500

    Métodos Get and Set "Principio de Ocultación"

--- src/poo/Automovil.java
+++ src/poo/Automovil.java
@@ -1,25 +1,98 @@
 package poo;
 
 public class Automovil {
-    String fabricante;
-    String modelo;
-    String color = "gris";
-    double cilindrada;
+    private String fabricante;
+    private String modelo;
+    private String color = "gris";
+    private double cilindrada;
+
+    private int capacidadEstanque = 40;
 
     /*Metodo no recomendable, ya que no recomienda las impresiones en las clases*/
     public void detalle(){
-        System.out.println("automovil.fabricante = " + this.fabricante);
-        System.out.println("auto.modelo = "+this.modelo);
-        System.out.println("automovil.color = " + this.color);
-        System.out.println("automovil.cilindrada = " + this.cilindrada);
+        System.out.println("automovil.fabricante = " + this.getFabricante());
+        System.out.println("auto.modelo = "+this.getModelo());
+        System.out.println("automovil.color = " + this.getColor());
+        System.out.println("automovil.cilindrada = " + this.getCilindrada());
     }
 
     public String detalle2(){
         StringBuilder sb =new StringBuilder();
-        sb.append("automovil.fabricante = " + this.fabricante);
-        sb.append("\nauto.modelo = "+this.modelo);
-        sb.append("\nautomovil.color = " + this.color);
-        sb.append("\nautomovil.cilindrada = " + this.cilindrada);
+        sb.append("automovil.fabricante = " + this.getFabricante());
+        sb.append("\nauto.modelo = "+this.getModelo());
+        sb.append("\nautomovil.color = " + this.getColor());
+        sb.append("\nautomovil.cilindrada = " + this.getCilindrada());
         return  sb.toString();
+    }
+    /*Metodo mejorado, nota como StrignBuilder es para manejar grandes cadenas que concatenar,
+    podria usarse sin problemas una variable de tipo String*/
+
+    public String verDetalle(){
+        return  "automovil.fabricante = " + this.getFabricante() +
+                "\nauto.modelo = " + this.getModelo() +
+                "\nautomovil.color = " + this.getColor() +
+                "\nautomovil.cilindrada = " + this.getCilindrada();
+    }
+
+    public String acelerar(int rpm){
+        return "El auto "+this.getFabricante()+" acelerando a "+rpm+"rpm";
+    }
+
+    public String frenar(){
+         return  this.getFabricante() + " "+this.getModelo()+ " frenando!";
+    }
+
+    public String acelerarFrenar(int rpm){
+        String acelerar = this.acelerar(rpm);
+        String frenar = this.frenar();
+        return  acelerar + "\n"+frenar;
+    }
+
+    public float calcularConsumo(int km, float porcentajeBencina){
+        return km/(this.getCapacidadEstanque()*porcentajeBencina);
+    }
+
+    public float calcularConsumo(int km, int porcentajeBencina){
+        return km/(this.getCapacidadEstanque()*(porcentajeBencina/100f));
+    }
+
+    public String getFabricante() {
+        return fabricante;
+    }
+
+    public void setFabricante(String fabricante) {
+        this.fabricante = fabricante;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public double getCilindrada() {
+        return cilindrada;
+    }
+
+    public void setCilindrada(double cilindrada) {
+        this.cilindrada = cilindrada;
+    }
+
+    public int getCapacidadEstanque() {
+        return capacidadEstanque;
+    }
+
+    public void setCapacidadEstanque(int capacidadEstanque) {
+        this.capacidadEstanque = capacidadEstanque;
     }
 }
--- src/poo/EjemploAutomovil.java
+++ src/poo/EjemploAutomovil.java
@@ -3,19 +3,27 @@ package poo;
 public class EjemploAutomovil {
     public static void main(String[] args) {
         Automovil subaru = new Automovil();
-        subaru.fabricante = "Subaru";
-        subaru.modelo = "Impreza";
-        subaru.cilindrada = 2.0;
-        subaru.color = "Blanco";
+        subaru.setFabricante("Subaru");
+        subaru.setModelo("Impreza");
+        subaru.setCilindrada(2.0);
+        subaru.setColor("Blanco");
 
         Automovil mazda = new Automovil();
-        mazda.fabricante = "Mazda";
-        mazda.modelo = "BT-50";
-        mazda.cilindrada = 3.0;
-        mazda.color = "Rojo";
+        mazda.setFabricante("Mazda");
+        mazda.setModelo("BT-50");
+        mazda.setCilindrada(3.0);
+        mazda.setColor("Rojo");
 
         System.out.println(mazda.detalle2());
         System.out.println();
         System.out.println(subaru.detalle2());
+        System.out.println();
+        System.out.println(subaru.verDetalle());
+        System.out.println(subaru.acelerar(3000));
+        System.out.println(subaru.frenar());
+
+        System.out.println(mazda.acelerarFrenar(4000));
+        System.out.println("Kilometros por litro: "+subaru.calcularConsumo(300,0.75f));
+        System.out.println("Kilometros por litro: "+subaru.calcularConsumo(300,75));
     }
 }
